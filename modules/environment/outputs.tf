1de4ce193faa3811709ed0decda064f0c5618f41
Marc Lamy <29371248+marclamy@users.noreply.github.com>
2025-03-16 18:46:07 +0100

    setup environment outputs

--- modules/environment/outputs.tf
+++ modules/environment/outputs.tf
@@ -1,3 +1,7 @@
 output "shared_project_id" {
   value = google_project.shared_project.project_id
 }
+
+output "shared_vpc_id" {
+  value = google_compute_network.vpc.id
+}
---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "prd_shared_project_id" {
+  value = module.env_prd.shared_project_id
+}
+
+output "prd_shared_vpc_id" {
+  value = module.env_prd.shared_vpc_id
+}
