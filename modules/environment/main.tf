bd656d05faa50fc884768af0dc6e709dca467511
Marc Lamy <29371248+marclamy@users.noreply.github.com>
2025-03-16 10:19:19 +0100

    add the platforms folder to the environments

--- modules/environment/main.tf
+++ modules/environment/main.tf
@@ -3,3 +3,9 @@ resource "google_folder" "env_folder" {
   display_name        = "${var.name}"
   deletion_protection = false # TODO: remove after POC
 }
+
+resource "google_folder" "env_platforms_folder" {
+  parent              = google_folder.env_folder.id
+  display_name        = "platforms"
+  deletion_protection = false # TODO: remove after POC
+}
