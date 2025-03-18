490446edafa11d590c5cd76ed2f8fa5db72dd858
Marc Lamy <29371248+marclamy@users.noreply.github.com>
2025-03-18 23:35:01 +0100

    add the platforms folder id to outputs

--- modules/environment/outputs.tf
+++ modules/environment/outputs.tf
@@ -5,3 +5,7 @@ output "shared_project_id" {
 output "shared_vpc_id" {
   value = google_compute_network.vpc.id
 }
+
+output "platforms_folder_id" {
+  value = google_folder.env_platforms_folder.folder_id
+}
--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "prd_shared_project_id" {
 output "prd_shared_vpc_id" {
   value = module.env_prd.shared_vpc_id
 }
+
+output "prd_platforms_folder_id" {
+  value = module.env_prd.platforms_folder_id
+}
