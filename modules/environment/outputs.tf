8ce334da0dfd6f5094c27a724c127dd91052cb15
Marc Lamy <29371248+marclamy@users.noreply.github.com>
2025-03-18 23:35:01 +0100

    add outputs for shared vpc host and platforms folder

--- modules/environment/outputs.tf
+++ modules/environment/outputs.tf
@@ -2,6 +2,10 @@ output "shared_project_id" {
   value = google_project.shared_project.project_id
 }
 
-output "shared_vpc_id" {
-  value = google_compute_network.vpc.id
+output "shared_vpc_host_project_id" {
+  value = google_compute_shared_vpc_host_project.vpc_host.project
+}
+
+output "platforms_folder_id" {
+  value = google_folder.env_platforms_folder.folder_id
 }
--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "prd_shared_project_id" {
   value = module.env_prd.shared_project_id
 }
 
-output "prd_shared_vpc_id" {
-  value = module.env_prd.shared_vpc_id
+output "prd_shared_vpc_host_project_id" {
+  value = module.env_prd.shared_vpc_host_project_id
+}
+
+output "prd_platforms_folder_id" {
+  value = module.env_prd.platforms_folder_id
 }
